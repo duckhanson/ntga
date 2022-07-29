9dd37a67db6fa8f4eb89b499e477df4d48714fe4
lucuslu <lucuslu@mxic.com.tw>
2022-07-29 14:08:38 +0800

    test keras

--- tf_test.py
+++ tf_test.py
@@ -1,10 +1,4 @@
 import tensorflow as tf
+import tensorflow.keras as k
 
 print(tf.config.list_physical_devices('GPU'))
-gpu_available = tf.test.is_gpu_available()
-is_cuda_gpu_available = tf.test.is_gpu_available(cuda_only=True)
-is_cuda_gpu_min_3 = tf.test.is_gpu_available(True, (3,0))
-
-print(gpu_available)
-print(is_cuda_gpu_available)
-print(is_cuda_gpu_min_3)
