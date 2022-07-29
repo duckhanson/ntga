1f89ac7653b6d8e6c23a3237538e6e91382c6340
lucuslu <lucuslu@mxic.com.tw>
2022-07-29 16:01:41 +0800

    try to fix gpu preallocate memory

--- util/run.sh
+++ util/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-k8srun -n ntga -p "source /share/anaconda3/bin/activate; conda activate lucus-nt; python nt_test.py"
+k8srun -n ntga -p "source /share/anaconda3/bin/activate; conda activate lucus-nt; python generate_attack.py --model_type fnn --dataset mnist --save_path ./data/"
