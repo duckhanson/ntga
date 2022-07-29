ba7df22c8ba7cee1c1aa9fde31b7b627ed9b9098
lucuslu <lucuslu@mxic.com.tw>
2022-07-29 14:03:29 +0800

    tf gpu test

---
+++ tf_test.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+gpu_available = tf.test.is_gpu_available()
+is_cuda_gpu_available = tf.test.is_gpu_available(cuda_only=True)
+is_cuda_gpu_min_3 = tf.test.is_gpu_available(True, (3,0))
+
+print(gpu_available)
+print(is_cuda_gpu_available)
+print(is_cuda_gpu_min_3)
