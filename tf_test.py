2379ae61bdfe9d6c54d2871adf4c34e23c0a4834
lucuslu <lucuslu@mxic.com.tw>
2022-07-29 14:06:40 +0800

    add phy device tf

--- tf_test.py
+++ tf_test.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+print(tf.config.list_physical_devices('GPU'))
 gpu_available = tf.test.is_gpu_available()
 is_cuda_gpu_available = tf.test.is_gpu_available(cuda_only=True)
 is_cuda_gpu_min_3 = tf.test.is_gpu_available(True, (3,0))
