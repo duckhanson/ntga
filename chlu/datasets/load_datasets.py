218733335c1e40eb4f7d60eb25ce96884cc1a334
lucuslu <lucuslu@mxic.com.tw>
2022-08-01 08:54:54 +0800

    pytorch dataloader

---
+++ chlu/datasets/load_datasets.py
@@ -0,0 +1,10 @@
+import torchvision
+
+cifar10_data = torchvision.datasets.CIFAR10('/share/lucuslu/ntga/chlu/datasets', download=True)
+data_loader = torch.utils.data.DataLoader(cifar10_data,
+                                          batch_size=16,
+                                          shuffle=True,
+                                          drop_last=True,
+                                          num_workers=2)
+
+print(cifar10_data.shape)
