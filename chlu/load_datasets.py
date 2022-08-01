a9de96894c18c6f0209f5352a3e99971c3e052ee
lucuslu <lucuslu@mxic.com.tw>
2022-08-01 09:50:12 +0800

    specify import lib

--- chlu/load_datasets.py
+++ chlu/load_datasets.py
@@ -1,13 +1,14 @@
-import torch
-import torchvision
+from torch.utils.data import DataLoader, random_split
+from torchvision import datasets
+from torchvision.transforms import ToTensor
 
 
 def load_datasets(name: str = 'cifar10'):
     print("Loading dataset...")
-    train_data = torchvision.datasets.CIFAR10('/share/lucuslu/ntga/chlu/datasets', train=True, download=True, transform=ToTensor())
+    train_data = datasets.CIFAR10('/share/lucuslu/ntga/chlu/datasets', train=True, download=True, transform=ToTensor())
     print("===train data===")
     print(train_data)
-    test_data = torchvision.datasets.CIFAR10('/share/lucuslu/ntga/chlu/datasets', train=False, download=True, transform=ToTensor())
+    test_data = datasets.CIFAR10('/share/lucuslu/ntga/chlu/datasets', train=False, download=True, transform=ToTensor())
     print("===test data===")
     print(test_data)
 
@@ -15,24 +16,24 @@ def load_datasets(name: str = 'cifar10'):
     # val_size = int(0.1 * len(full_dataset))
     # test_size = len(full_dataset) - train_size - val_size
 
-    # train_data, val_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, val_size, test_size])
+    # train_data, val_dataset, test_dataset = random_split(full_dataset, [train_size, val_size, test_size])
     # print("===train data===")
     # print(train_dataset)
     # print("===val data===")
     # print(val_dataset)
     # print("===test data===")
     # print(test_dataset)
-    train_loader = torch.utils.data.DataLoader(train_data,
+    train_loader = DataLoader(train_data,
                                             batch_size=16,
                                             shuffle=True,
                                             drop_last=True,
                                             num_workers=2)
-    # val_loader = torch.utils.data.DataLoader(val_dataset,
+    # val_loader = DataLoader(val_dataset,
     #                                         batch_size=16,
     #                                         shuffle=False,
     #                                         drop_last=True,
     #                                         num_workers=2)
-    test_loader = torch.utils.data.DataLoader(test_data,
+    test_loader = DataLoader(test_data,
                                             batch_size=16,
                                             shuffle=False,
                                             drop_last=True,
