88ace6678147a8aba120fe41b97eb0f99c7e1c37
lucuslu <lucuslu@mxic.com.tw>
2022-08-01 10:05:10 +0800

    test import lib

--- chlu/load_datasets.py
+++ chlu/load_datasets.py
@@ -3,7 +3,7 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 
 
-def load_datasets(name: str = 'cifar10'):
+def load_datasets(name: str = 'cifar10', batch_size: int = 64, num_workers: int = 16):
     print("Loading dataset...")
     train_data = datasets.CIFAR10('/share/lucuslu/ntga/chlu/datasets', train=True, download=True, transform=ToTensor())
     print("===train data===")
@@ -12,31 +12,15 @@ def load_datasets(name: str = 'cifar10'):
     print("===test data===")
     print(test_data)
 
-    # train_size = int(0.7 * len(full_dataset))
-    # val_size = int(0.1 * len(full_dataset))
-    # test_size = len(full_dataset) - train_size - val_size
-
-    # train_data, val_dataset, test_dataset = random_split(full_dataset, [train_size, val_size, test_size])
-    # print("===train data===")
-    # print(train_dataset)
-    # print("===val data===")
-    # print(val_dataset)
-    # print("===test data===")
-    # print(test_dataset)
     train_loader = DataLoader(train_data,
-                                            batch_size=16,
+                                            batch_size=batch_size,
                                             shuffle=True,
                                             drop_last=True,
-                                            num_workers=2)
-    # val_loader = DataLoader(val_dataset,
-    #                                         batch_size=16,
-    #                                         shuffle=False,
-    #                                         drop_last=True,
-    #                                         num_workers=2)
+                                            num_workers=num_workers)
     test_loader = DataLoader(test_data,
-                                            batch_size=16,
+                                            batch_size=batch_size,
                                             shuffle=False,
                                             drop_last=True,
-                                            num_workers=2)
+                                            num_workers=num_workers)
     print("Fin Loading dataset (split into train, test)")
     return train_loader, test_loader
