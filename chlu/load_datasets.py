803ea839d60b049cfcc645bb3cf5b23c19598da3
lucuslu <lucuslu@mxic.com.tw>
2022-08-01 09:32:26 +0800

    train split @load_datasets

--- chlu/load_datasets.py
+++ chlu/load_datasets.py
@@ -1,11 +1,38 @@
 import torch
 import torchvision
 
-cifar10_data = torchvision.datasets.CIFAR10('/share/lucuslu/ntga/chlu/datasets', download=True)
-data_loader = torch.utils.data.DataLoader(cifar10_data,
-                                          batch_size=16,
-                                          shuffle=True,
-                                          drop_last=True,
-                                          num_workers=2)
 
-print(cifar10_data)
+def load_datasets(name: str = 'cifar10'):
+    print("Loading dataset...")
+    full_dataset = torchvision.datasets.CIFAR10('/share/lucuslu/ntga/chlu/datasets', download=True)
+    print("===full data===")
+    print(full_dataset)
+
+    train_size = int(0.7 * len(full_dataset))
+    val_size = int(0.1 * len(full_dataset))
+    test_size = len(full_dataset) - train_size - val_size
+
+    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, val_size, test_size])
+    print("===train data===")
+    print(train_dataset)
+    print("===val data===")
+    print(val_dataset)
+    print("===test data===")
+    print(test_dataset)
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                            batch_size=16,
+                                            shuffle=True,
+                                            drop_last=True,
+                                            num_workers=2)
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                            batch_size=16,
+                                            shuffle=False,
+                                            drop_last=True,
+                                            num_workers=2)
+    test_loader = torch.utils.data.DataLoader(test_dataset,
+                                            batch_size=16,
+                                            shuffle=False,
+                                            drop_last=True,
+                                            num_workers=2)
+    
+    return train_loader, val_loader, test_loader
