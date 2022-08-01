5b7e75d9b4bbac4701f953795e985d1cb9886060
lucuslu <lucuslu@mxic.com.tw>
2022-08-01 16:11:01 +0800

    print x_train np shape

--- generate_attack.py
+++ generate_attack.py
@@ -46,7 +46,7 @@ def main(t: int = 64, nb_iter: int = 10, model_type: str = 'fnn', block_size: in
     x_train_adv = []
     y_train_adv = []
     for batch, ((_x_train, _y_train), (x_val, y_val)) in tqdm(enumerate(zip(train_data, val_data))):
-
+        print(_x_train.detach().numpy().shape)
         _x_train_adv = projected_gradient_descent(model_fn=model_fn, kernel_fn=kernel_fn, grads_fn=grads_fn, 
                                                   x_train=_x_train.detach().numpy(), y_train=_y_train.detach().numpy(), x_test=x_val.detach().numpy(), y_test=y_val.detach().numpy(), 
                                                   t=t, loss='cross-entropy', eps=eps, eps_iter=eps_iter, 
