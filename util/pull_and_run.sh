f62cb3e31c2044286358bf1db002be7ddf5bc07b
lucuslu <lucuslu@mxic.com.tw>
2022-08-02 13:29:32 +0800

    add env setting: pre allocate mem

--- util/pull_and_run.sh
+++ util/pull_and_run.sh
@@ -3,4 +3,7 @@ clear
 git fetch
 git pull
 k8sdel ntga
+export TF_FORCE_UNIFIED_MEMORY=1
+export XLA_PYTHON_CLIENT_PREALLOCATE=false
+export XLA_PYTHON_CLIENT_ALLOCATOR=platform
 k8srun -n ntga -p "source /share/anaconda3/bin/activate; conda activate lucus-nt; python tf_test.py"
