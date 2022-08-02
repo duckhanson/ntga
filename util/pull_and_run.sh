10e055eab2d6b796c44d509a9c5830b3464238d2
lucuslu <lucuslu@mxic.com.tw>
2022-08-02 11:19:22 +0800

    fix run file

--- util/pull_and_run.sh
+++ util/pull_and_run.sh
@@ -3,4 +3,4 @@ clear
 git fetch
 git pull
 k8sdel ntga
-k8srun -n ntga -p "source /share/anaconda3/bin/activate; conda activate lucus-nt; python generate_attack.py"
+k8srun -n ntga -p "source /share/anaconda3/bin/activate; conda activate lucus-nt; python tf_test.py"
