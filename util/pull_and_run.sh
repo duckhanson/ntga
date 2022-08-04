c1cea574e1460f7b10c0520a1f084b1e7568846f
lucuslu <lucuslu@mxic.com.tw>
2022-08-04 11:25:41 +0800

    comment export @ pull and run

--- util/pull_and_run.sh
+++ util/pull_and_run.sh
@@ -3,7 +3,7 @@ clear
 git fetch
 git pull
 k8sdel ntga
-export TF_FORCE_UNIFIED_MEMORY=1
-export XLA_PYTHON_CLIENT_PREALLOCATE=false
-export XLA_PYTHON_CLIENT_ALLOCATOR=platform
+# export TF_FORCE_UNIFIED_MEMORY=1
+# export XLA_PYTHON_CLIENT_PREALLOCATE=false
+# export XLA_PYTHON_CLIENT_ALLOCATOR=platform
 /share/lucuslu/ntga/util/run.sh
