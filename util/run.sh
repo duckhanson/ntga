829511cb322009d8fd71b3098087c54ed0b37fc1
lucuslu <lucuslu@mxic.com.tw>
2022-08-04 11:24:48 +0800

    update run.sh

--- util/run.sh
+++ util/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=$CONDA_PREFIX/lib
+./share/lucuslu/ntga/util/install.sh
 k8srun -n ntga -p "source /share/anaconda3/bin/activate; conda activate lucus-nt; python generate_attack.py --model_type fnn --dataset cifar10 --save_path /share/lucuslu/ntga/chlu/datasets"
 
 # python generate_attack.py --model_type fnn --dataset cifar10 --save_path /share/lucuslu/ntga/chlu/datasets/
